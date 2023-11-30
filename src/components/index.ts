e120ad766fb8784e08f04bb592641ed1c90584fa
nuhptr <nugrahaadi733@gmail.com>
2023-11-30 23:19:13 +0700

    components/add and update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import { Icons } from "./icons"
 import NavItems from "./nav-items"
+import NavItem from "./nav-item"
 import Navbar from "./navbar"
 import MaxWidthWrapper from "./max-width-wrapper"
 
-export { Icons, NavItems, Navbar, MaxWidthWrapper }
+export { Icons, NavItems, Navbar, MaxWidthWrapper, NavItem }
--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -1,3 +1,36 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/constant"
+import { useState } from "react"
+import { NavItem } from "@/components"
+
 export default function NavItems() {
-   return <div></div>
+   const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+   const isAnyOpen = activeIndex !== null
+
+   return (
+      <div className="flex h-full gap-4">
+         {PRODUCT_CATEGORIES.map((category, index) => {
+            const handleOpen = () => {
+               if (activeIndex === index) setActiveIndex(null)
+               else setActiveIndex(index)
+            }
+
+            const close = () => setActiveIndex(null)
+
+            const isOpen = index === activeIndex
+
+            return (
+               <NavItem
+                  key={category.value}
+                  category={category}
+                  handleOpen={handleOpen}
+                  isOpen={isOpen}
+                  isAnyOpen={isAnyOpen}
+               />
+            )
+         })}
+      </div>
+   )
 }
