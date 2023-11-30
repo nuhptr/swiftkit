1924af4486f9dd27cc19557196a4f32b6b9be7a5
nuhptr <nugrahaadi733@gmail.com>
2023-11-30 19:42:29 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Building UI Kit with Next.js and Tailwind CSS for sell UI UX Design community.
+
+<!-- Image section -->
+
+## Getting Started
+
+    ## Dependencies
+    - shadcn-ui (https://ui.shadcn.com/) - npx shadcn-ui@latest init
+      - class-variance-authority
+      - lucide-react
+      - clsx
+      - tailwind-merge
+      - tailwindcss-animate
+    - tailwindcss (https://tailwindcss.com/) - npm install tailwindcss, npx tailwindcss init -p
+
+## Features
+
+## Tech Stack
---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+   title: "SwiftKIT | Swift UI Kit for Web",
+   description: "SwiftKIT is a UI Kit for Web, built from figma, Sketch, and Invision.",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <html lang="en">
+         <body className={inter.className} suppressHydrationWarning>
+            {children}
+         </body>
+      </html>
+   )
+}
